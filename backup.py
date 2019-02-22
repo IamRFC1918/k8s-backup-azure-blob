c3b5063eb34ff99292498aac8a67467013178d21
Löppi <loeppi@Loeppinator.local>
2019-02-22 14:38:43 +0100

    imit

--- backup.py
+++ backup.py
@@ -2,6 +2,7 @@ import os
 import azure.storage.blob
 import re
 import yaml
+from datetime import datetime, timedelta
 
 #Get ENV secret variables
 account_name = os.environ['ACCOUNT_NAME']
@@ -16,6 +17,7 @@ with open('config.yml') as f:
 config_folder = config['k8s-backup-config']['Backup_Dir']
 config_filter = config['k8s-backup-config']['Filter']
 config_blob_folder = config['k8s-backup-config']['Blob_Folder']
+retention_days = config['k8s-backup-config']['retention_days']
 
 #Get File list by Filter
 def getfiles(folder, filter):
@@ -40,6 +42,16 @@ def uploadFiles(folder, file, connection, container, prefix, blobs):
     else:
     #Upload File
         connection.create_blob_from_path(container, blobpath, source_file)
+        print("Will now upload blob " + blobpath)
+
+#Erase Files older than specified in config
+def deleteFiles(connection, blobs, container, retention):
+    for blob in blobs:
+        time_diff = datetime.now() - blob.properties.last_modified
+        if time_diff>retention:
+            connection.delete_blob(container, blob.name)
+            print("Deleted Blob " + blob.name + " because of retention time")    
+
 
 #init Blob Connection
 block_blob_service = azure.storage.blob.BlockBlobService(account_name=account_name, account_key=account_key)
@@ -48,3 +60,4 @@ filteredfiles = getfiles(config_folder, config_filter)
 blobs = getblobs(block_blob_service, container_name)
 for file in filteredfiles:
     uploadFiles(config_folder, file, block_blob_service, container_name, config_blob_folder, blobs)
+delete = deleteFiles(block_blob_service, blobs, container_name, retention_days)
